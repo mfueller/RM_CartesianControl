23d07e597185fc151c216208e68dd6ccf07f8da9
Marc Wollenweber <marcwollenweber@web.de>
2012-12-07 11:34:54 +0100

    Delete build and bin folders and add it to .gitignore

---
+++ hbrs_cc_library/common/src/hbrs_cc_library.cpp
@@ -0,0 +1,22 @@
+#include "hbrs_cc_library.h"
+
+
+Hbrs_cc_Library::Hbrs_cc_Library(std::string armDescriptionFile)
+{
+	
+    /* init and configure here */
+}
+
+Hbrs_cc_Library::~Hbrs_cc_Library()
+{
+    /* clean up here */
+}
+
+std::vector<double> Hbrs_cc_Library::getJointVelocities() {
+	
+	std::vector<double> jointVelocities;
+	return jointVelocities;
+}
+
+
+
